67eab1aadc032d1c679f795f5e1c5e48d5165c90
blpupleb <dsw61116@student.dsw.edu.pl>
2026-05-01 13:00:00 +0200

    fixed

--- Assets/SceneOpener.cs
+++ Assets/SceneOpener.cs
@@ -6,7 +6,7 @@ public class SceneOpener : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OpenScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName); 
+        SceneManager.LoadScene(sceneName);
     }
 
     // Update is called once per frame
